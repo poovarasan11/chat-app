b4d94bd66bac8b1d1a261d1686d3c56ec15c083f
Poovarasan <poovarasan1999skp@gmail.com>
2024-08-08 17:59:07 +0530

    profile name add

--- client/src/pages/CheckEmailPage.jsx
+++ client/src/pages/CheckEmailPage.jsx
@@ -1,7 +1,81 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { IoCloseOutline } from "react-icons/io5";
+import { Link, useNavigate } from "react-router-dom";
+import uploadFile from "../helpers/UploadeFile";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { PiUserCircle } from "react-icons/pi";
 const CheckEmailPage = () => {
-  return <div>CheckEmailPage</div>;
+  const [data, setData] = useState({
+    email: "",
+  });
+
+  const navigate = useNavigate();
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const URL = `${process.env.REACT_APP_BACKEND_URL}/api/email`;
+    try {
+      const response = await axios.post(URL, data);
+      toast.success(response.data.message);
+      if (response.data.success) {
+        setData({
+          email: "",
+        });
+        navigate("/password", {
+          state: response?.data,
+        });
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
+
+  return (
+    <div className="mt-4">
+      <div className="bg-white w-full max-w-md  rounded overflow-hidden p-4 mx-auto ">
+        <div className="w-fit mx-auto mb-2">
+          <PiUserCircle size={80} />
+        </div>
+        <h3>Welcome to chat app!</h3>
+        <form className="grid gap-3 mt-3" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              className="bg-slate-100 px-2 py-1 focus:outline-primary"
+              value={data.email}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+
+          <button className="bg-primary text-lg px-4 py-1 hover:bg-secondary rounded mt-2 font-bold text-white leading-relaxed tracking-wide">
+            Login
+          </button>
+          <p className="my-1 text-center">
+            Create New User ?
+            <Link to={"/register"} className="hover:text-primary font-semibold">
+              Register
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default CheckEmailPage;
